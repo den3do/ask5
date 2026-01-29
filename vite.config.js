2b98a7340f8dbd31e5cbb53a5c4d9043ed73fec7
den3do <78220372+den3do@users.noreply.github.com>
2026-01-29 22:44:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ask5/', // 這裡一定要跟你的儲存庫名稱一模一樣
+  base: '/ask5/', 
 })
