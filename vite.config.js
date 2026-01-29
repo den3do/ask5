a13526d51d5f3324aa786cc40bcfc33db801354c
den3do <78220372+den3do@users.noreply.github.com>
2026-01-29 22:25:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ask5/', // 注意：這裡要填入你的 Repository 名稱
+  base: './', // 改成相對路徑，適應 GitHub Pages 的子目錄
 })
